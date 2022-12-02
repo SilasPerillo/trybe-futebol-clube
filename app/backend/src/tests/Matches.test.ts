1feebdcbfd1c9dcf76e7d5cf56d90a257b854951
SilasPerillo <silasperillo@gmail.com>
2022-12-02 01:11:45 -0300

    test: verifica retorno de sucesso na rota get /matches

---
+++ app/backend/src/tests/Matches.test.ts
@@ -0,0 +1,36 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { Response } from 'superagent'
+import { AllMatches } from './mock/Matches.mock'
+
+import App from '../app';
+import Matches from '../database/models/Matches';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('Teste para rota de partidas', () => {
+  let chaiHttpResponse: Response;
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Verifica retorno 200 no caso de sucesso', async () => {
+    sinon
+    .stub(Matches, 'findAll')
+    .resolves(AllMatches as any);
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(AllMatches);
+  })
+})
---
+++ app/backend/src/tests/mock/Matches.mock.ts
@@ -0,0 +1,189 @@
+const AllMatches = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      id: 16,
+      teamName: "São Paulo"
+    },
+    teamAway: {
+      id: 8,
+      teamName: "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeam": 9,
+    "homeTeamGoals": 1,
+    "awayTeam": 14,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "id": 9,
+      "teamName": "Internacional"
+    },
+    "teamAway": {
+      "id": 14,
+      "teamName": "Santos"
+    }
+  },
+  {
+    "id": 3,
+    "homeTeam": 4,
+    "homeTeamGoals": 3,
+    "awayTeam": 11,
+    "awayTeamGoals": 0,
+    "inProgress": false,
+    "teamHome": {
+      "id": 4,
+      "teamName": "Corinthians"
+    },
+    "teamAway": {
+      "id": 11,
+      "teamName": "Napoli-SC"
+    }
+  },
+  {
+    "id": 4,
+    "homeTeam": 3,
+    "homeTeamGoals": 0,
+    "awayTeam": 2,
+    "awayTeamGoals": 0,
+    "inProgress": false,
+    "teamHome": {
+      "id": 3,
+      "teamName": "Botafogo"
+    },
+    "teamAway": {
+      "id": 2,
+      "teamName": "Bahia"
+    }
+  },
+  {
+    "id": 5,
+    "homeTeam": 7,
+    "homeTeamGoals": 1,
+    "awayTeam": 10,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "id": 7,
+      "teamName": "Flamengo"
+    },
+    "teamAway": {
+      "id": 10,
+      "teamName": "Minas Brasília"
+    }
+  }
+];
+
+const AllMatchesInProgress = [
+  {
+    "id": 1,
+    "homeTeam": 16,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": true,
+    "teamHome": {
+      "id": 16,
+      "teamName": "São Paulo"
+    },
+    "teamAway": {
+      "id": 8,
+      "teamName": "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+
+    "homeTeam": 9,
+    "homeTeamGoals": 1,
+    "awayTeam": 14,
+    "awayTeamGoals": 1,
+    "inProgress": true,
+    "teamHome": {
+      "id": 9,
+      "teamName": "Internacional"
+    },
+    "teamAway": {
+      "id": 14,
+      "teamName": "Santos"
+    }
+  },
+  {
+    "id": 3,
+    "homeTeam": 4,
+    "homeTeamGoals": 3,
+    "awayTeam": 11,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "id": 4,
+      "teamName": "Corinthians"
+    },
+    "teamAway": {
+      "id": 11,
+      "teamName": "Napoli-SC"
+    }
+  }
+];
+
+const AllMatchesFinished = [
+  {
+    "id": 1,
+    "homeTeam": 16,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "id": 16,
+      "teamName": "São Paulo"
+    },
+    "teamAway": {
+      "id": 8,
+      "teamName": "Grêmio"
+    }
+  },
+  ,
+  {
+    "id": 2,
+
+    "homeTeam": 9,
+    "homeTeamGoals": 1,
+    "awayTeam": 14,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "id": 9,
+      "teamName": "Internacional"
+    },
+    "teamAway": {
+      "id": 14,
+      "teamName": "Santos"
+    }
+  },
+  {
+    "id": 3,
+    "homeTeam": 4,
+    "homeTeamGoals": 3,
+    "awayTeam": 11,
+    "awayTeamGoals": 0,
+    "inProgress": false,
+    "teamHome": {
+      "id": 4,
+      "teamName": "Corinthians"
+    },
+    "teamAway": {
+      "id": 11,
+      "teamName": "Napoli-SC"
+    }
+  }
+];
+
+export { AllMatches, AllMatchesInProgress, AllMatchesFinished };
