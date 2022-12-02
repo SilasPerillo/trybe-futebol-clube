8ae95aeb12493715d206859463bdeeaea1c4d553
SilasPerillo <silasperillo@gmail.com>
2022-12-02 02:07:05 -0300

    test: Verifica retorno de sucesso na rota get /matches?inProgress

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -3,7 +3,7 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import { Response } from 'superagent'
-import { AllMatches } from './mock/Matches.mock'
+import { AllMatches, AllMatchesInProgress } from './mock/Matches.mock'
 
 import App from '../app';
 import Matches from '../database/models/Matches';
@@ -21,7 +21,7 @@ describe('Teste para rota de partidas', () => {
     sinon.restore();
   });
 
-  it('Verifica retorno 200 no caso de sucesso', async () => {
+  it('Verifica retorno 200 no caso de sucesso na rota /matches', async () => {
     sinon
     .stub(Matches, 'findAll')
     .resolves(AllMatches as any);
@@ -33,4 +33,18 @@ describe('Teste para rota de partidas', () => {
     expect(chaiHttpResponse.status).to.be.eq(200);
     expect(chaiHttpResponse.body).to.be.deep.eq(AllMatches);
   })
+
+  it('Verifica retorno 200 no caso de sucesso na rota /matches?inProgress', async () => {
+    sinon
+    .stub(Matches, 'findAll')
+    .resolves(AllMatchesInProgress as any);
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matches?inProgress=true');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(AllMatchesInProgress);
+  })
+
 })
